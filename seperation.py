f4a81709ddbdb99677ecc3a1c731456c79ae0e9f
Lungelo Packery <81719132+Lungelo63@users.noreply.github.com>
2022-05-03 23:36:06 +0200

    Add files via upload

---
+++ seperation.py
@@ -0,0 +1,4 @@
+sentence = input("Enter a sentence: ")  #Taking in the user input
+
+sentence = "\n".join(sentence.split())  #spliting  'sentence' than joining a new line
+print(sentence)                         #printing
